2354d0149b8dd2139c590ab4f564df6024d85c4b
dadanhan <hansmnc@gmail.com>
2020-08-24 17:22:25 +0100

    properly display image on screen and convert everything to 16-bit

--- openImage.py
+++ openImage.py
@@ -14,12 +14,17 @@ import sys
 
 def main(argv):
     
-    im = cv2.imread(argv[1],-1)
-
-    print(im)
-
-    cv2.imshow('test',im)
-    cv2.waitKey(0)
+    im = cv2.imread(argv[1],-1)     #read in image
+    im_scaled = np.array(cv2.normalize(im,dst=None,alpha=0,beta=65535,norm_type=cv2.NORM_MINMAX),dtype='uint16')     #scale image
+    #print values of im
+    print(im_scaled)
+    #display image on screen
+    winName = "test"        #display window name
+    cv2.namedWindow(winName,cv2.WINDOW_NORMAL)      #create window
+    cv2.resizeWindow(winName,600,600)       #resize window
+    cv2.imshow(winName,im_scaled)       #show image
+    cv2.waitKey(0)      #delete window if button pressed
+    cv2.destroyAllWindows()     #free all windows
 
 if __name__=="__main__":
     main(sys.argv)
