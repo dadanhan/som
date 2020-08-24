2005394c758ee1ad03e042bfb4e2f60855318787
dadanhan <hansmnc@gmail.com>
2020-08-24 17:02:43 +0100

    second commit

--- openImage.py
+++ openImage.py
@@ -1,8 +1,25 @@
+# pushing to git is 
+# git status
+# git add filename.ext
+# git commit -m "Some Message"
+# git push origin master
+
+#run script using python3 
+
+#SOM implementation by Daniel Han 24/8/2020
+
 import cv2
 import numpy as np
+import sys
 
-im = cv2.imread('test1.tif',-1)
-print(im)
+def main(argv):
+    
+    im = cv2.imread(argv[1],-1)
 
-cv2.imshow('test',im)
-cv2.waitKey(0)
+    print(im)
+
+    cv2.imshow('test',im)
+    cv2.waitKey(0)
+
+if __name__=="__main__":
+    main(sys.argv)
