6ac235bdf388195c115ddc0aaa6a9b9303b51deb
dadanhan <hansmnc@gmail.com>
2020-08-25 22:38:03 +0100

    began competitive learning function, initialization of random weights

--- openImage.py
+++ openImage.py
@@ -19,26 +19,33 @@ def show_image(winName,image,width,height):
     cv2.waitKey(0)      #delete window if button pressed
     cv2.destroyAllWindows()     #free all windows
 
+def competitive_learn_image(image,ksize,nrv,kernel):
+    #get shape of image
+    r,c = image.shape
+    #create a kernel for looking at a local area in image
+    gap = int(np.ceil(ksize/2 -1))
+    somim = np.zeros((r-gap,c-gap))
+    #initialize reference vectors with random numbers in refv between lowest and highest values in image
+    refv = np.random.randint(np.amin(image),np.amax(image),size=(nrv,ksize*ksize))
+
+    print(refv)
+    print(np.amax(image),np.amin(image))
+
+    return somim
+
 def main(argv):
     im = cv2.imread(argv[1],-1)     #read in image
     im_scaled = np.array(cv2.normalize(im,dst=None,alpha=0,beta=65535,norm_type=cv2.NORM_MINMAX),dtype='uint16')     #scale image
-   
-    #print values of im
-    # print(im_scaled)
-    #get shape of im
-    r,c = im.shape
-    print(r,c)
 
     #display image on screen
     show_image("test",im_scaled,600,600)
 
-    #create a kernel for looking at a local area in image
+    #competitive learning algorithm for an image
     ksize = 3
-    kernel = np.ones((ksize,ksize))
-    gap = int(np.ceil(ksize/2 -1))
-    somim = np.zeros((r-gap,c-gap))
-    #initialize reference vectors
+    nrv = 4
+    somim = competitive_learn_image(im,ksize,nrv,kernel=np.ones((ksize,ksize)))
     
+
 
 if __name__=="__main__":
     main(sys.argv)
